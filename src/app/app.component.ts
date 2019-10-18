7b008d0fced0da2c6a8a596980cb8c50c7741908
Enzo Palermo <epalermo@utbm.fr>
2019-10-18 11:51:36 +0200

    début front-end Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private counterService: CounterService){}
+
   title = 'counters';
+
+  reset() {
+    this.initialValue = [0,0,0];
+  }
+  
 }
