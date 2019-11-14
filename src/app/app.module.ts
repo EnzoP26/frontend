fb5bdbd9806859e959be6006e0434141ce2c483b
Enzo Palermo <epalermo@utbm.fr>
2019-11-14 16:52:05 +0100

    maj counter-detail.cmp.ts + imports d'app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { CounterComponent } from './counter/counter.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
+
+import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
@@ -32,6 +36,7 @@ import { CounterDetailComponent } from './counter-detail/counter-detail.componen
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
+    MatMenuModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
--- src/app/counter-detail/counter-detail.component.ts
+++ src/app/counter-detail/counter-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Counter } from '../counter';
+import { ActivatedRoute } from '@angular/router';
+import { CounterService } from '../counter.service';
+import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'app-counter-detail',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterDetailComponent implements OnInit {
 
-  constructor() { }
+  counter: Counter;
+  constructor(
+    private route : ActivatedRoute,
+    private counterService: CounterService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getCounter();
+  }
+
+  getCounter() : void{
+    this.route.params.subscribe(
+      params => {
+        this.counterService.getCounterValue(params['id'])
+          .subscribe(counter => this.counter = counter);
+      }
+    );
+  } 
+  increment() {
+    this.counterService.increment(this.counter.id)
+      .subscribe(counter => {
+        this.counter.value = counter.value;
+      });
   }
 
 }
