bcd3ab82f7119d472925d60b481123ce58b648ac
Enzo Palermo <epalermo@utbm.fr>
2019-11-15 11:46:20 +0100

    add fct increment

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,30 +1,47 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Counter } from './counter';
-import { Observable } from 'rxjs';
+import { Observable, Subscription  } from 'rxjs';
+import { ActionCableService, Channel } from 'angular2-actioncable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
   public initialValue = [12, 5, 7];
+  subscription: Subscription;
+
   private counterObservable: Map<number,Observable<Counter>> = new Map() ;
 
-  constructor(private http: HttpClient,/*private cableServicebbbite: NgxActionCableService*/) { }
+  constructor(private http: HttpClient,private cableService: ActionCableService) { 
 
-  reset() {
-    this.initialValue = [0, 0, 0];
-  }
+  const channel: Channel = this.cableService
+  .cable('wss://lp4asgadot.herokuapp.com/cable')
+  .channel('CountersChannel', {});
+
+ 
+  this.subscription = channel.received().subscribe(message => {
+    
+  console.log(message, this.counterObservable[message.id]);
+
+    if(this.counterObservable.has(message.id)) {
+      console.log("emit");
+      this.counterObservable[message.id].emit({id: message.id, name: message.name, value:message.value})
+    }
+  });
+}
+
 /*
   increment(position: number): number {
     this.initialValue[position]++;
     return this.initialValue[position];
   }*/
+ reset() {
+    this.initialValue = [0, 0, 0];
+  }
 
-
-  increment(id : number): Observable<Counter>{
-    this.http.patch("https://lp4asgadot.herokuapp.com/counters/"+id+".json",{"value" : 1}).subscribe();
-    return this.http.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json");
+  increment(position: number) {
+    this.http.patch<any>(".netlify/functions/increment", {counterId: position}).subscribe()
   }
 
   getCounter(id: number): Observable<Counter> {
